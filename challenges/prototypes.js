ea7addd9d489660f530a21b86373c14db985d54e
UnknownMonk <smodcartoon@gmail.com>
2019-01-25 13:28:46 -0500

    completed for loop section

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -39,7 +39,7 @@ CuboidMaker.prototype.surfaceArea = function() {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-let cuboid = new CuboidMaker({
+const cuboid = new CuboidMaker({
   length: 4,
   width: 5,
   height: 5
